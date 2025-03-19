2fefa700201c6eb4ceb8067ade12da518f529993
LuckyOx04 <ivailon27@gmail.com>
2025-03-19 15:28:17 +0200

    Started adding constants

--- SAP-WebAPI/obj/Debug/net9.0/SAP-WebAPI.AssemblyInfo.cs
+++ SAP-WebAPI/obj/Debug/net9.0/SAP-WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b81fe0fb99bfa25b5012e0fb01ed6746fbb170d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb00bc2338087f8b9284cf368249bb2a947bb1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
