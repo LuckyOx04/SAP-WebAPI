845c6c825a6cb84e4da3168bec646613a2f04f50
LuckyOx04 <ivailon27@gmail.com>
2025-03-19 22:26:09 +0200

    Added the first controller as well as a xUnit test project

--- SAP-ClassLibrary/obj/Debug/net9.0/SAP-ClassLibrary.AssemblyInfo.cs
+++ SAP-ClassLibrary/obj/Debug/net9.0/SAP-ClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAP-ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d6724ba7d46d5e6288c549eff388c232a35831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4449ef28ef9f3c056b1af1ddf1562ffa583873")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAP-ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAP-ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Sap-WebAPI-Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Sap_WebAPI_Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true, "should be true.");
+    }
+}
