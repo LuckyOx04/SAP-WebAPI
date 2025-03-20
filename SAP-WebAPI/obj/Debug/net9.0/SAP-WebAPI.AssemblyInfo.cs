9ae10a05527b1a1f5f0d884e01231026cae0f8ab
LuckyOx04 <ivailon27@gmail.com>
2025-03-20 13:09:15 +0200

    First commit

--- SAP-WebAPI/obj/Debug/net9.0/SAP-WebAPI.AssemblyInfo.cs
+++ SAP-WebAPI/obj/Debug/net9.0/SAP-WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfb95af9c12a2205cb4a56df7d28fe25f2f9a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4449ef28ef9f3c056b1af1ddf1562ffa583873")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAP-WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
